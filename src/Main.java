47dbd6c84868ae9213792c0df878492c4a05f89c
Itzik Fadida <49044964+ItzikFa94@users.noreply.github.com>
2021-11-09 21:39:50 +0200

    jar

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args){
 
-        DataBaseConnection dbConnection = new DataBaseConnection("String", "Barak", "8995");
+        DataBaseConnection dbConnection = new DataBaseConnection("jdbc:oracle:thin:@ora1.ise.bgu.ac.il:1521/ORACLE", "yitzhakh", "abcd");
 
         dbConnection.fileToDataBase("assets/films.csv");
 
