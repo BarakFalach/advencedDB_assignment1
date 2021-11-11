1b6e03f5d4aef6e94c70b6be9a078de4cb492711
barak falach <barak8995@gmail.com>
2021-11-11 18:07:31 +0200

    setting

--- src/DataBaseConnection.java
+++ src/DataBaseConnection.java
@@ -2,17 +2,21 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class DataBaseConnection {
     private final String connectionString;
     private final String userName;
     private final String password;
+    private int maxDistance;
 
     public DataBaseConnection(String connectionString, String userName, String password) {
         this.connectionString = connectionString;
         this.userName = userName;
         this.password = password;
+        this.maxDistance = 0;
     }
 
     public void fileToDataBase(String path) {
@@ -40,11 +44,18 @@ public class DataBaseConnection {
     }
 
     public void calculateSimilarity() {
+        List<Long> movieIds = new ArrayList<>();
         Connection dbConnection = createConnection();
+        float currentSimilarity = 0;
         if (dbConnection == null) return;
-        int maximalDistance = maximalDistance(dbConnection);
-
-
+        calculateMaximalDistance(dbConnection);
+        movieIds = getAllMovieIDs(dbConnection);
+        // loop on loop
+        int i = 0;
+        int j = 1;
+        currentSimilarity = getSingleSimilarity(dbConnection,movieIds.get(i), movieIds.get(j));
+        writeSingleSimilarity(dbConnection,movieIds.get(i), movieIds.get(j),currentSimilarity);
+        // end of loop
     }
 
     public void printSimilarItems(long mid) {
@@ -64,18 +75,43 @@ public class DataBaseConnection {
         return null;
     }
 
-    private int maximalDistance(Connection dbConnection) {
-        if (dbConnection == null) return 0;
-        try {
-            CallableStatement callableStatement = dbConnection.prepareCall("{call MaximalDistance()}");
-            callableStatement.execute();
-            callableStatement.registerOutParameter(1, Types.INTEGER);
-            return callableStatement.getInt(1);
+    private void calculateMaximalDistance(Connection dbConnection) {
+        if (dbConnection != null) {
+            try {
+                CallableStatement callableStatement = dbConnection.prepareCall("{? = call MaximalDistance}");
+                callableStatement.execute();
+                callableStatement.registerOutParameter(1, Types.INTEGER);
+                this.maxDistance = callableStatement.getInt(1);
+            }
+            catch (SQLException throwable) {
+                throwable.printStackTrace();
+            }
+        }
+    }
 
+    private List<Long> getAllMovieIDs(Connection dbConnection) {
+        return null;
+
+
+    }
+
+    private float getSingleSimilarity(Connection dbConnection, long mid1, long mid2) {
+        try {
+            CallableStatement callableStatement = dbConnection.prepareCall("");
         } catch (SQLException throwables) {
             throwables.printStackTrace();
         }
         return 0;
+    }
+
+    private void writeSingleSimilarity(Connection dbConnection, long mid1, long mid2, float similarity) {
+        try {
+            CallableStatement callableStatement = dbConnection.prepareCall("");
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+
     }
 
 
